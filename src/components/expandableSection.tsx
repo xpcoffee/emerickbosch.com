a5b04a916f7c9d20d6707835b883645bd4690e47
Emerick Bosch <xpc.dev@gmail.com>
2021-09-10 11:10:10 +0200

    Use the details tag for expandable sections
    #builtinfunctionality

--- src/components/expandableSection.tsx
+++ src/components/expandableSection.tsx
@@ -35,23 +35,14 @@ export const ExpandableSection = ({
   description,
   initiallyHidden: initiallyClosed = false,
 }: Props) => {
-  const [expanded, setExpanded] = useState(!initiallyClosed)
-  const toggleExpanded = () => setExpanded(prevValue => !prevValue)
-  const icon = expanded ? faCaretDown : faCaretRight
-  const contentDisplay = expanded ? "block" : "hidden"
-
   return (
-    <div>
-      {headerElementWrapper(
-        <button className="flex" onClick={toggleExpanded}>
-          <div className="w-5 mr-2">
-            <FontAwesomeIcon icon={icon} />
-          </div>
-          {header}
-        </button>
-      )}
-      <div className="ml-8 quicksand">{description}</div>
-      <div className={`${contentDisplay} ml-8 mt-4 mb-4`}>{children}</div>
-    </div>
+    <details className="mb-4">
+      <summary className="cursor-pointer">
+        {headerElementWrapper(<>{header}</>)}
+        <br />
+        <div className="quicksand">{description}</div>
+      </summary>
+      <div className={`ml-8 mt-4 mb-4`}>{children}</div>
+    </details>
   )
 }
