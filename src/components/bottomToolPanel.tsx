b28cde7b377394367a1ede8fbbe349007bcdc449
Emerick Bosch <emerick.bosch@gmail.com>
2022-06-26 16:09:36 +0000

    Rework bottom toolbar
    Overlay wasn't scrolling correctly on mobile.

--- src/components/bottomToolPanel.tsx
+++ src/components/bottomToolPanel.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react"
+import { FAIcon, getIcon } from "../utils/fontAwesome"
 
 const BottomToolPanel = ({
   children,
@@ -43,13 +44,20 @@ const BottomToolPanel = ({
       <div
         className={
           classNames +
-          " z-20 absolute left-0 right-0 top-0 bottom-0 h-max p-4 mb-0 mt-auto" +
-          " drop-shadow border-gray-300 border-t-1 bg-white" +
-          " flex items-center"
+          //   " z-20 absolute left-0 right-0 top-0 bottom-0 h-max p-4 mb-0 mt-auto" +
+          " z-20 fixed h-full w-full left-0 top-0" +
+          " drop-shadow border-gray-300 border-t-1 bg-white overflow-y-auto"
+          //   " flex items-center"
         }
         ref={ref}
       >
-        {children}
+        <button
+          className="fixed text-xl right-10 top-5"
+          onClick={() => onDismiss?.()}
+        >
+          {getIcon("faTimes" as FAIcon)}
+        </button>
+        <div className="relative top-10 w-full py-5">{children}</div>
       </div>
     </>
   )
