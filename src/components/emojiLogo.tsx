e8a135a6a622990e672e6c38438988412aac033e
Rick Bosch <emerick.bosch@gmail.com>
2022-07-17 12:25:31 +0000

    Make logo link to main page

--- src/components/emojiLogo.tsx
+++ src/components/emojiLogo.tsx
@@ -1,9 +1,12 @@
+import { Link } from "gatsby"
 import React from "react"
 
 const EmojiLogo = ({ className }: { className?: string }) => {
   return (
     <div className={`${className} flex justify-center`}>
-      <div className="text-sm text-gray-600 quicksand">☕ ⌨️ ⚙️</div>
+      <Link to="/">
+        <div className="text-sm text-gray-600 quicksand">☕ ⌨️ ⚙️</div>
+      </Link>
     </div>
   )
 }
