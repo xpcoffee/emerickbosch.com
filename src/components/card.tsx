89686531de1f6abb2d1846b5ab69f50de6c94fef
Rick Bosch <emerick.bosch@gmail.com>
2022-07-24 10:56:16 +0000

    Update personal tenets

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,11 +6,13 @@ type Props = PropsWithChildren<{
 
 const Card = ({ children, heading }: Props) => {
   const header = heading ? (
-    <h2 className="text-blue-800 mt-2 text-xl">{heading}</h2>
+    <h2 className="text-blue-800 dark:text-orange-300 mt-2 text-xl">
+      {heading}
+    </h2>
   ) : undefined
 
   return (
-    <div className="border-blue-600 border-l-4 my-5 flex">
+    <div className="border-blue-600 dark:border-orange-400 border-l-4 my-5 flex">
       <div className="w-10"></div>
       <div>
         {header}
