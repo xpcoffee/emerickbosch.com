39eec642c9066ca7dd0530ec20b125f62f690ab9
Emerick Bosch <emerick.bosch@gmail.com>
2023-11-26 15:57:36 +0000

    Use NODE_ENV when determining plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,87 @@
+const isDev = process.env['NODE_ENV'] ===  'development'
+
+const devTooling = isDev ? [
+    {
+      /**
+       * Extract/generate types from source code (e.g. creates types for graphQL queries)
+       */
+      resolve: `gatsby-plugin-typegen`,
+      options: {
+        emitSchema: {
+          "src/__generated__/gatsby-introspection.json": true,
+          "src/__generated__/gatsby-schema.graphql": true,
+        },
+        emitPluginDocuments: {
+          "src/__generated__/gatsby-plugin-documents.graphql": true,
+        },
+      },
+    },
+] : []
+
+const deployment = isDev ? [] : [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "emerickbosch.com",
+      },
+    },
+]
+
+const imageProcessing = [
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      /**
+       * Surface images to graphql. Used to enable SVGs to be used as favicon.
+       * https://jackwarren.info/posts/guides/gatsby/svg-favicon/
+       */
+      resolve: `gatsby-source-filesystem`,
+      options: { name: `images`, path: `${__dirname}/src/images` },
+    },
+    {
+      /**
+       * Enable SVGs to be imported as components
+       */
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
+]
+
+const markdown = [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/src/articles`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 600,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+]
+
+const styles = [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-fontawesome-css`,
+]
+
 module.exports = {
   siteMetadata: {
     title: `Emerick Bosch`,
@@ -25,87 +109,10 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    /**
-     * Developer tooling
-     */
-    {
-      /**
-       * Extract/generate types from source code (e.g. creates types for graphQL queries)
-       */
-      resolve: `gatsby-plugin-typegen`,
-      options: {
-        emitSchema: {
-          "src/__generated__/gatsby-introspection.json": true,
-          "src/__generated__/gatsby-schema.graphql": true,
-        },
-        emitPluginDocuments: {
-          "src/__generated__/gatsby-plugin-documents.graphql": true,
-        },
-      },
-    },
-    /**
-     * Image processing
-     */
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      /**
-       * Surface images to graphql. Used to enable SVGs to be used as favicon.
-       * https://jackwarren.info/posts/guides/gatsby/svg-favicon/
-       */
-      resolve: `gatsby-source-filesystem`,
-      options: { name: `images`, path: `${__dirname}/src/images` },
-    },
-    {
-      /**
-       * Enable SVGs to be imported as components
-       */
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /images/,
-        },
-      },
-    },
-    /**
-     * Markdown (MDX) plugins
-     */
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `articles`,
-        path: `${__dirname}/src/articles`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 600,
-            },
-          },
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-prismjs`,
-        ],
-      },
-    },
-    /**
-     * CSS plugins
-     */
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-fontawesome-css`,
-    /**
-     * Deployment
-     */
-    {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-        bucketName: "emerickbosch.com",
-      },
-    },
+    ...devTooling,
+    ...imageProcessing,
+    ...markdown,
+    ...styles,
+    ...deployment
   ],
 }
