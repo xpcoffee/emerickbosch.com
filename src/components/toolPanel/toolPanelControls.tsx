a0438a3b4ae441dba372f9eca773f1fcdcc68024
Emerick Bosch <xpc.dev@gmail.com>
2024-02-18 14:32:26 +0000

    Feb 18, 2024, 2:32 PM

--- src/components/toolPanel/toolPanelControls.tsx
+++ src/components/toolPanel/toolPanelControls.tsx
@@ -31,14 +31,13 @@ const ToolPanelControls = ({
   }
 
   return (
-    <div className={" bottom-0 right-0 left-0 z-20 fixed"}>
+    <div className={" bottom-0 right-0 left-0 z-20 fixed flex-col"}>
       <div
         className={
           className +
           " z-10 fixed w-full left-0 top-0" +
           " drop-shadow border-gray-300 border-t-1 bg-white overflow-y-auto" +
           " dark:bg-slate-800" +
-          " relative top-10 w-full py-5" +
           " md:sticky md:top-0 md:pt-8 md:overflow-y-auto" +
           `${!showPanel ? " block md:hidden" : ""}`
         }
