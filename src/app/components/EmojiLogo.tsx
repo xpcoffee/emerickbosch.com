f618f676310cf2f1d73d065d4e0689ca9c33800c
Emerick Bosch <xpc.dev@gmail.com>
2024-07-01 11:39:39 +0200

    Make emojilogo go to repo

--- src/app/components/EmojiLogo.tsx
+++ src/app/components/EmojiLogo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const EmojiLogo = ({ className }: { className?: string }) => {
   return (
     <div className={`${className} flex justify-center`}>
-      <Link href="/">
+      <Link href="https://github.com/xpcoffee/emerickbosch.com">
         <div className="quicksand text-sm text-gray-600">☕ ⌨️ ⚙️</div>
       </Link>
     </div>
